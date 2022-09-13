e5c82d25e7fa9c4381d0af707c8ebdad17cf970f
gastonlista <gastonorigin1@hotmail.com>
2022-09-13 16:11:11 -0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//funcion que muestra el menu responsive
+function responsiveMenu(){
+    let x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+};
